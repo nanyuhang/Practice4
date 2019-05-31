947f954786fa2d704aec929950a91ff9e8859084
Nanyuhang <1976377686@qq.com>
2019-05-31 21:04:40 +0800

    OperatorJavaFile
    Simple example

---
+++ operator.java
@@ -0,0 +1,19 @@
+package practice4;
+
+public class operator {
+	public operator() {
+		
+	}
+	public  int add(int a, int b) {
+		return a + b;
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x, y;
+		x = 2;
+		y = 4;
+		operator operator1=new operator();
+		System.out.println(operator1.add(x, y));
+	}
+
+}

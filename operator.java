74ef75474a24e7b4e28db8a30521442eff64095e
Nanyuhang <1976377686@qq.com>
2019-05-31 21:38:51 +0800

    Add Multiply
    Multiply

--- operator.java
+++ operator.java
@@ -10,13 +10,17 @@ public class operator {
 	public  int min(int a, int b) {
 		return a - b;
 	}
+	public  int multiply(int a, int b) {
+	
+		return a * b;
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int x, y;
 		x = 2;
 		y = 4;
 		operator operator1=new operator();
-		System.out.println(operator1.min(x, y));
+		System.out.println(operator1.multiply(x, y));
 	}
 
 }
